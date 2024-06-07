6dd174ee149c33a1624bba3e674ace7bcc453bbb
chenxiaoyao6228 <chenxiaoyao6228@163.com>
2024-06-07 18:55:47 +0800

    docs: update

--- Editor/_demo/monaco-editor/src/app/editor/code-editor.service.ts
+++ Editor/_demo/monaco-editor/src/app/editor/code-editor.service.ts
@@ -1,26 +1,50 @@
-import { Injectable } from '@angular/core';
-import * as monaco from 'monaco-editor';
+// code-editor.service.ts
+import { Inject, Injectable, InjectionToken, Optional } from '@angular/core';
+import { AsyncSubject, Subject } from 'rxjs';
+
+// 根据使用时部署情况可能会需要更改资源路径
+export const APP_MONACO_BASE_HREF = new InjectionToken<string>(
+  'appMonacoBaseHref'
+);
 
 @Injectable({
   providedIn: 'root'
 })
 export class CodeEditorService {
-  private editorInstance: monaco.editor.IStandaloneCodeEditor | null = null;
+  private afterScriptLoad$: AsyncSubject<boolean> = new AsyncSubject<boolean>();
+  private isScriptLoaded = false;
 
-  constructor() { }
-
-  initializeEditor(element: HTMLElement, options: monaco.editor.IStandaloneEditorConstructionOptions): void {
-    this.editorInstance = monaco.editor.create(element, options);
+  constructor(@Optional() @Inject(APP_MONACO_BASE_HREF) private base: string) {
+    this.loadMonacoScript();
   }
 
-  getEditorInstance(): monaco.editor.IStandaloneCodeEditor | null {
-    return this.editorInstance;
+  public getScriptLoadSubject(): AsyncSubject<boolean> {
+    return this.afterScriptLoad$;
   }
 
-  disposeEditor(): void {
-    if (this.editorInstance) {
-      this.editorInstance.dispose();
-      this.editorInstance = null;
+  public loadMonacoScript(): void {
+    // 通过 AMD 的方式加载 monaco 脚本
+    const onGotAmdLoader: any = () => {
+      // load monaco here
+      (<any>window).require.config({
+        paths: { vs: `${this.base || 'assets/vs'}` }
+      });
+      (<any>window).require(['vs/editor/editor.main'], () => {
+        this.isScriptLoaded = true;
+        this.afterScriptLoad$.next(true);
+        this.afterScriptLoad$.complete();
+      });
+    };
+
+    // 在这里会需要加载到 monaco 的 loader.js
+    if (!(<any>window).require) {
+      const loaderScript = document.createElement('script');
+      loaderScript.type = 'text/javascript';
+      loaderScript.src = `${this.base || 'assets/vs'}/loader.js`;
+      loaderScript.addEventListener('load', onGotAmdLoader);
+      document.body.appendChild(loaderScript);
+    } else {
+      onGotAmdLoader();
     }
   }
 }
--- Editor/_demo/monaco-editor/src/app/editor/editor.component.ts
+++ Editor/_demo/monaco-editor/src/app/editor/editor.component.ts
@@ -1,10 +1,28 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  EventEmitter,
+  forwardRef,
+  HostBinding,
+  Input,
+  OnDestroy,
+  Output,
+  Renderer2,
+  ViewChild,
+  effect,
+  signal,
+} from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { fromEvent, Subject, takeUntil } from 'rxjs';
 import { CodeEditorService } from './code-editor.service';
 
+declare const monaco: any;
+
 @Component({
-  selector: 'app-code-editor',
   standalone: true,
-  imports: [],
+  selector: 'app-code-editor',
   template: `<div #editorContainer class="editor-container"></div>`,
   styles: [`
     .editor-container {
@@ -13,22 +31,120 @@ import { CodeEditorService } from './code-editor.service';
       left: 0;
       width: 50%;
       height: 100%;
+      border: 1px solid blue;
     }
-  `]
+  `],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => EditorComponent),
+      multi: true,
+    },
+  ],
 })
-export class EditorComponent {
-  @ViewChild('editorContainer', { static: true }) editorContainer!: ElementRef;
+export class EditorComponent implements AfterViewInit, OnDestroy, ControlValueAccessor {
+  @Input() options: any;
+  @Input() @HostBinding('style.height') height: string = '100%';
+  @Output() readonly editorInitialized: EventEmitter<any> = new EventEmitter<any>();
+  @ViewChild('editorContainer', { static: true }) editorContentRef!: ElementRef;
 
-  constructor(private codeEditorService: CodeEditorService) { }
+  private destroy$: Subject<void> = new Subject<void>();
+  private _editor: any = undefined;
+  private _valueSignal = signal<string>('');
+  private _disposables: any[] = [];
 
-  ngAfterViewInit(): void {
-    this.codeEditorService.initializeEditor(this.editorContainer.nativeElement, {
-      language: 'javascript',
-      theme: 'vs-dark'
+  onChange = (_: any) => { };
+  onTouched = () => { };
+
+  constructor(
+    private codeEditorService: CodeEditorService,
+    private renderer: Renderer2
+  ) {
+    effect(() => {
+      const value = this._valueSignal();
+      if (this._editor && this._editor.getModel()) {
+        this._editor.getModel().setValue(value);
+      }
     });
   }
 
+  // 双向绑定设置 editor 内容
+  writeValue(value: string): void {
+    this._valueSignal.set(value || '');
+  }
+
+  // 通过 onChange 方法将改变后的值传出，即 ngModelChange
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  ngAfterViewInit(): void {
+    this.codeEditorService
+      .getScriptLoadSubject()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((isLoaded) => {
+        if (isLoaded) {
+          this.initMonaco();
+        }
+      });
+
+    // 监听浏览器窗口大小，做 editor 的自适应
+    fromEvent(window, 'resize')
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(() => {
+        if (this._editor) {
+          this._editor.layout();
+        }
+      });
+  }
+
+  private initMonaco(): void {
+    const options = this.options;
+    const language = this.options['language'];
+    const editorDiv: HTMLDivElement = this.editorContentRef.nativeElement;
+
+    if (!this._editor) {
+      this._editor = monaco.editor.create(editorDiv, options);
+      this._editor.setModel(monaco.editor.createModel(this._valueSignal(), language));
+      this.editorInitialized.emit(this._editor);
+      this.renderer.setStyle(
+        this.editorContentRef.nativeElement,
+        'height',
+        this.height
+      );
+      this.setValueEmitter();
+      this._editor.layout();
+    }
+  }
+
+  private setValueEmitter() {
+    if (this._editor) {
+      const model = this._editor.getModel();
+      this._disposables.push(
+        model.onDidChangeContent(() => {
+          const value = model.getValue();
+          this._valueSignal.set(value);
+          this.onChange(value);
+        })
+      );
+    }
+  }
+
   ngOnDestroy(): void {
-    this.codeEditorService.disposeEditor();
+    this.destroy$.next();
+    this.destroy$.complete();
+    if (this._editor) {
+      this._editor.dispose();
+      this._editor = undefined;
+    }
+    if (this._disposables.length) {
+      this._disposables.forEach((disposable) => disposable.dispose());
+      this._disposables = [];
+    }
   }
 }
